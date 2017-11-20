ff69ccc9aac450e2331a88830ada82fdfe306160
laihhi <mylaizhicheng@qq.com>
2017-11-20 19:43:20 +0800

    修改：     package.json 	修改：     public/index.html 	修改：     src/index.js 	删除：     src/title.js 	修改：     yarn.lock
     尚未暂存以备提交的变更：
    	删除：     ../test/.babelrc
    	删除：     ../test/dev-server.js
    	删除：     ../test/package.json
    	删除：     ../test/public/index.html
    	删除：     ../test/public/js/main.js
    	删除：     ../test/src/component/Lzc.js
    	删除：     ../test/src/main.js
    	删除：     ../test/webpack.config.js
    	删除：     ../test/yarn.lock

--- redux/src/index.js
+++ redux/src/index.js
@@ -1,12 +1,99 @@
-import React from 'react';
-import ReactDOM ,{render} from 'react-dom';
+import React  from 'react';
+import {PropTypes} from 'prop-types';
+import ReactDOM ,{ render} from 'react-dom';
+import {createStore} from 'redux';
+import {Provider,connect} from 'react-redux';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import './title.js';
 
 
+class Test extends React.Component{
+  static propTypes = {
+    username:PropTypes.String
+  }
+  render(){
+    return (
+      <div>
+        <h1>lzccheng</h1>
+      </div>
+    )
+  }
+}
+
+const state = {
+  username:'lzccheng',
+  content:'gitgitigt'
+}
+let reducer = (state={},action)=>{
+  switch (action.type){
+    case 'update_content':
+      return {
+        ...state,
+        content:action.content
+      };
+      case 'update_username':
+        return {
+          ...state,
+          username:action.username
+        }
+      case 'update_themeColor':
+        return {
+          ...state,
+          themeColor:action.themeColor
+        }
+      default:
+        return state;
+  }
+}
+const action = {
+  type:'',
+  content:''
+}
+const store = createStore(reducer);
+console.log(store);
+console.log(store.getState());
+store.dispatch({type:'update_content',content:'gitgitgit'})
+store.dispatch({type:'update_username',username:'cheng'})
+console.log(store.getState());
+
+console.log(connect);
+let mapStateToProps = (state)=>{
+  return {
+    username:state.username
+  }
+}
 
 
+class Content extends React.Component {
+  static propTypes = {
+    themeColor: PropTypes.string
+  }
 
+  render () {
+    return (
+      <div>
+        <p style={{ color: this.props.themeColor }}>React.js 小书内容</p>
+      </div>
+    )
+  }
+}
+
+const a = (state) => {
+  return {
+    themeColor: state.themeColor
+  }
+}
+const dis = (dis)=>{
+  return {
+    onClick:()=>{
+      dis({type:'update_themeColor',themeColor:'red'})
+    }
+  }
+}
+Content = connect(a)(Content)
+
+render(<Provider store={store}><Content /></Provider>,document.getElementById('box'));
+
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
